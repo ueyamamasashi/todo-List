d0e25393769bdd5a587aa89411de0db42bc5aff3
上山雅士 <ueyamamasashi@ueyamamasashi.local>
2021-01-13 21:04:21 +0900

    second

--- script.js
+++ script.js
@@ -4,32 +4,50 @@ const trigger = document.getElementById("trigger");
 const addValue = document.getElementById("addValue");
 const tBody = document.getElementById("tbody");
 let counter = 0;
-const addTask = (task) => {
+//task add
+const addTask = (task) => { 
+    const params = {
+        'No'       : counter,
+        'content'  : task,
+        'condition': conditionBtn(),
+        'delete'   : deleteBtn()
+    }
+    counter++;
+    return params;
+}
+//todo表示
+const displayTask = (params) => {
     const newTable = document.createElement('tr');
-    const newId = document.createElement('th');
-    const newTask = document.createElement('th');
-    const newCondition = document.createElement('th');
-    const newBtn = document.createElement('button');
-    const newDeleteBtn = document.createElement('button');
-    newBtn.innerText = '作業中';
-    newDeleteBtn.innerText = '削除';
-
-    newId.innerText = counter;
-    newTask.innerText = task;
-    newCondition.appendChild(newBtn);
-    newCondition.appendChild(newDeleteBtn);
+    const newId = document.createElement('td');
+    const newTask = document.createElement('td');
+    const newCondition = document.createElement('td');
+    newId.innerText = params['No'];
+    newTask.innerText = params['content'];
     newTable.appendChild(newId);
     newTable.appendChild(newTask);
+    newCondition.appendChild(params['condition']);
+    newCondition.appendChild(params['delete']);
     newTable.appendChild(newCondition);
     tBody.appendChild(newTable);
-    counter++;
-   
-    
 }
 
+//conditionBtn
+const conditionBtn = () => {
+    const newBtn = document.createElement('button');
+    newBtn.innerText = '作業中'; //soon boolen
+    return newBtn;
+}
+
+//deleteBtn
+const deleteBtn = () => {
+    const newDeleteBtn = document.createElement('button');    
+    newDeleteBtn.innerText = '削除';
+    return newDeleteBtn;
+}
 
 trigger.addEventListener('click', (event) => {
     const task = addValue.value;
-    addTask(task);
+    const params = addTask(task);
+    displayTask(params);
     addValue.value = '';
 });
